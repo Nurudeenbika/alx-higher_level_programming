779684cf297b365fcf73ae35771e766981d0f1a0
Nurudeen Hassan <nurudeenbika@gmail.com>
2022-06-07 15:42:48 -0700

    1-last_digit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 """prints the ASCII alphabet, in lowercase, not followed by a new line."""
 
 for letter in range(97, 123):
-    print(f"{}".format(chr(letter)) end="")
+    print("{}".format(chr(letter)) end="")
