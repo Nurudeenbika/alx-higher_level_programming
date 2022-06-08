29bde3a8a53996649adfce9709f04ef6a5ee1f52
Nurudeen Hassan <nurudeenbika@gmail.com>
2022-06-08 10:12:44 -0700

    7-islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Hassan Nurudeen
+"""function that check for lower character"""
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
