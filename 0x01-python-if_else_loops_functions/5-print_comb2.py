355306dc9197ed2e6dd25b67522cd0b1b8953a7d
Nurudeen Hassan <nurudeenbika@gmail.com>
2022-06-07 23:26:07 -0700

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# Author - Hassan Nurudeen
-"""print numbers from 0 to 99, it should all be in two digits"""
 for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
