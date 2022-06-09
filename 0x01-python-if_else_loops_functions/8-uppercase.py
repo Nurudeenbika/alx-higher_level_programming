b7441bf036fbf6bfc9f3bbda02fbcfbea450b8fa
Nurudeen Hassan <nurudeenbika@gmail.com>
2022-06-09 00:11:53 -0700

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # Author - Hassan Nurudeen
-
 """print a string in uppercase followed by a new line"""
 
-def uppercase(str):
+
+def uppercase(str):    
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c))
+            c = chr(ord(c) - 32)
         print("{}".format(c), end="")
     print("")
