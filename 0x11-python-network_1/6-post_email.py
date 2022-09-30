fb324b039910f65f6de116467724fe444ab021cd
Hassan Nurudeen <nurudeenbika@gmail.com>
2022-09-30 07:19:46 +0100

    python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
+"""Sends a form data to a URL."""
 import sys
-import urllib.request
+import requests
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        form_data = [('email', email)]
+        response = requests.post(url, data=form_data)
+        print(response.text)
